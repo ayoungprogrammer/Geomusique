03f7b2782ac05b3761be7ebfc0fb6dd42d7318ba
Michael <michaelyoung1995@gmail.com>
2014-05-17 21:23:01 -0700

    play song

--- api.js
+++ api.js
@@ -6,25 +6,29 @@ var User = mongoose.model('User',userSchema.UserSchema);
 
 mongoose.connect('mongodb://geomusique:michael123@oceanic.mongohq.com:10019/app25342036');
 
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("connected to db");
+});
+
 //Exports
-exports.save_song = function (req,res){
-	db.saveItem(req.body,function(err){
-		res.json(err);
-	});
-};
 
 exports.login_user = function(req,res){
 	var name = req.body.username;
 	console.log(name);
-	User.findOne({'username':name},function(err,person){
+	User.findOne({'username':name},function(err,user){
 		if(err){
+			console.error(err);
+		}
+		if(!user){
 			console.log("Creating user");
-			var newUser = new User({username:name,saves:[]});
+			var newUser = new User({username:name,saves:[],trips:[]});
 			newUser.save(function(err,obj){
 				if(err)return console.error(err);
 				req.session.username = name;
 				res.send('sucess');
-				//res.redirect('/');
+			//res.redirect('/');
 			});
 		}
 		else {
@@ -39,16 +43,46 @@ exports.login_user = function(req,res){
 };
 
 exports.saveSong = function(req,res){
-	
-	
-};
-
-exports.getSongs = function (req,res){
-	console.log("getting songs");
+	console.log("Saving song: "+req.session.username);
 	User.findOne({'username':req.session.username},function(err,user){
 		if(err){
 			return console.error(err);
 		}
-		return res.json(user.saves);
+		if(!user){
+			res("no user");
+			return;
+		}
+		user.saves.push(req.body.save);
+		user.save(req.body.song,function(err,song){
+			if(err){
+				return console.error(err);
+			}
+			res.json("success");
+		});
+	});
+};
+
+exports.saveTrip = function(req,res){
+	User.findOne({'username':req.session.username},function(err,user){
+		if(err){
+			return console.error(err);
+		}
+		user.trips.add(req.body.trips);
+		user.save();
+	});
+};
+
+exports.getSongs = function (req,res){
+	console.log("getting songs "+req.session.username);
+	User.findOne({'username':req.session.username},function(err,user){
+		if(err){
+			return console.error(err);
+		}
+		if(!user){
+			res.json("no user");
+			return;
+		}
+		console.log(user);
+		res.json(user.saves);
 	});
 };
